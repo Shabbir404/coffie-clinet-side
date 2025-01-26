55253684680a683fe09bf07ca533f921182fc09d
Shabbir Ahamed <shabbirkimgbrand55@gmail.com>
2025-01-26 23:21:19 +0600

    home page image showcase

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,8 @@
 import coffieIMG from '../../images/more/3.png'
+import coffie1 from '../../images/icons/1.png'
+import coffie2 from '../../images/icons/2.png'
+import coffie3 from '../../images/icons/3.png'
+import coffie4 from '../../images/icons/4.png'
 import './home.css'
 const Home = () => {
     return (
@@ -14,6 +18,32 @@ const Home = () => {
                     </div>
                 </div>
             </div>
+            {/* top discribe */}
+            <div className='w-full h-[250px] bg-[#ECEAE3] hidden md:block'>
+                <div className='w-8/12 mx-auto flex md:space-x-8 justify-center'>
+                    <div className='mt-12'>
+                        <img className='w-14 h-14' src={coffie1} alt="" />
+                        <h1 className='rancho-font text-[#331A15] text-3xl mt-2'>Awesome Aroma</h1>
+                        <p className='w-[301px]'>You will definitely be a fan of the design & aroma of your coffee</p>
+                    </div>
+                    <div className='mt-12'>
+                        <img className='w-14 h-14' src={coffie2} alt="" />
+                        <h1 className='rancho-font text-[#331A15] text-3xl mt-2'>High Quality</h1>
+                        <p className='w-[301px]'>We served the coffee to you maintaining the best quality</p>
+                    </div>
+                    <div className='mt-[49px]'>
+                        <img className='w-14 h-14' src={coffie3} alt="" />
+                        <h1 className='rancho-font text-[#331A15] text-3xl mt-2'>Pure Grades</h1>
+                        <p className='w-[301px]'>The coffee is made of the green coffee beans which you will love</p>
+                    </div>
+                    <div className='mt-12'>
+                        <img className='w-14 h-14' src={coffie4} alt="" />
+                        <h1 className='rancho-font text-[#331A15] text-3xl mt-2'>Proper Roasting</h1>
+                        <p className='w-[290px]'>Your coffee is brewed by first roasting the green coffee beans</p>
+                    </div>
+                </div>
+            </div>
+            {/* top discribe end */}
         </div>
     );
 };
