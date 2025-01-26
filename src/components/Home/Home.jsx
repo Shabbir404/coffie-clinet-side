ef5b791bd5b30c812e196b92569a1ce5a765c43f
Shabbir Ahamed <shabbirkimgbrand55@gmail.com>
2025-01-26 15:41:11 +0600

    home 1/3 design complete

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,11 +5,13 @@ const Home = () => {
         <div>
             <div className='w-full relative flex'>
                 <img className='w-full md:h-[640px]' src={coffieIMG} alt="" />
-                <div className='absolute  ml-[800px] mt-62 '>
-                    <h1 className='rancho-font md:text-5xl text-2xl text-white'>Would you like a Cup of Delicious Coffee?</h1>
-                    <p className='mt-5 text-white raleway-font md:w-[724px]'>It`s coffee time - Sip & Savor - Relaxation in every sip! Get the nostalgia back!! Your companion of every moment!!! Enjoy the beautiful moments and make them memorable.</p>
-                    <button className=' mt-5 pl-4 pr-4 pt-2 pb-2 text-xl rancho-font
+                <div className='absolute md:ml-[800px] md:mt-62 mt-10 '>
+                    <h1 className='rancho-font text-center md:text-left md:text-5xl text-2xl text-white'>Would you like a Cup of Delicious Coffee?</h1>
+                    <p className='text-center md:text-left md:mt-5 mt-2 text-white raleway-font md:w-[724px]'>It`s coffee time - Sip & Savor - Relaxation in every sip! Get the nostalgia back!! Your companion of every moment!!! Enjoy the beautiful moments and make them memorable.</p>
+                    <div className='justify-center flex md:justify-start'>
+                        <button className=' md:mt-5 mt-3 pl-4 pr-4 pt-2 pb-2 text-xl rancho-font
                      background-color'>Learn More</button>
+                    </div>
                 </div>
             </div>
         </div>
